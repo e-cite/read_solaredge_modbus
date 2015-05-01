886df1054329950f8c13dfd253123a29e8626040
e-cite <dev@andreas-dolp.de>
2015-05-01 17:20:29 +0200

    Added support for FIFO file buffers to upload the values using vzlogger file interface. Not implemented properly, but working in a first test :-)
    TODO: Add Error control, ID variables and timestamps.

--- src/read_solaredge_modbus_vzlogger.c
+++ src/read_solaredge_modbus_vzlogger.c
@@ -2,66 +2,45 @@
  * Project: 2014p03_stromzaehler-auswertung-SE3000
  * Filename: read_solaredge_modbus_vzlogger.c
  * Function: Reads SolarEdge SE3000 inverter via modbus and writes current data in a file, so vzlogger can read it
- * Comment: First implementation
+ * Comment: Implements communication to vzlogger using a tmp buffer file
  * Requires:
  * Parameters:
  * Return values:
  * Name: Andreas Dolp
- * Date: 13.11.2014
- * Version: 1.0
+ * Date: 01.05.2015
+ * Version: 2.0
  * --------------------------- */
 
 #include <stdio.h>
-#include <curl/curl.h>
 #include <errno.h>
 #include <unistd.h>
 #include <math.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
 #include "read_solaredge_modbus_vzlogger.h"
 #include "sunspec_modbus_mappings_solaredge.h"
 
+#define MAXLEN 16
+
 extern int errno;
-
-/*
- * @brief Generates a POST request in volkszaehler format to transmit cur_power to volkszaehler middleware
- * @param int cur_power = value to transmit, char *vz_uuid = volkszaehler channel uuid
- * @return Returns always the curl_easy_perform return value, CURLE_OK = 0 if successfull
- */
-int vz_post_value(int cur_power, char *vz_uuid)
-{
-	char vz_url[255];
-	char vz_postfields[50];
-	CURL *curl;
-	CURLcode res;
-
-	sprintf(vz_url,"%s/data/%s.json",VZ_MIDDLEWARE_URL,vz_uuid);
-	sprintf(vz_postfields,"value=%d",cur_power);
-
-	curl = curl_easy_init();
-	if (curl) {
-		curl_easy_setopt(curl,CURLOPT_URL,vz_url);
-		curl_easy_setopt(curl,CURLOPT_POSTFIELDS,vz_postfields);
-		res = curl_easy_perform(curl);
-		if(res != CURLE_OK)
-			fprintf(stderr,"Error while transmitting to POST request: %s",curl_easy_strerror(res));
-		curl_easy_cleanup(curl);
-	}
-
-	curl_global_cleanup();
-	return (int)res;
-}
-
 
 int main()
 {
 	int I_AC_Power_calculated = 0;
-	uint16_t I_Status_before = 4;
+	/* Structs for the involved inverters */
 	struct sunspec_modbus_mappings_solaredge *inv1 = malloc(MB_REG_COUNT*sizeof(uint16_t));
 	if ((void*)inv1 <= NULL) {
 		fprintf(stderr, "Error while allocating memory for sunspec_modbus_mappings_solaredge inv1: %s\n",strerror(errno));
 		return -1;
 	}
+
+	/* Create buffer file */
+	int fd = NULL;
+	char *path = "/tmp/vzloggerfifo1";
+	mkfifo(path, 0666);
+	char buffer[MAXLEN];
+
 
 	while(1) {
 		if (read_modbus_rtu_device((uint16_t *)inv1,MB_SLAVE_ADDRESS,MB_REG_ADDRESS,sizeof(struct sunspec_modbus_mappings_solaredge)/2) <= 0) {
@@ -73,23 +52,29 @@ int main()
 
 		I_AC_Power_calculated = inv1->I_AC_Power*pow(10,inv1->I_AC_Power_SF);
 		
-		if(inv1->I_Status == 4 || I_Status_before == 4) {
-			printf("Received the values listed below:\n");
-			printf("AC Power: %d\n",I_AC_Power_calculated);
-			printf("AC Voltage: %d\n",inv1->I_AC_VoltageAB);
-			printf("AC Current: %d\n",inv1->I_AC_Current);
-			printf("Heatsink temperature: %d\n",inv1->I_Temp_Heatsink);
-			if(vz_post_value(I_AC_Power_calculated, VZ_UUID) != 0)
-				continue;
-		} else {
-			printf("Nightmode or OFF detected, retrying in %d seconds...\n", SE_NIGHTMODE_READ_CYCLE_SECONDS);
-			sleep(SE_NIGHTMODE_READ_CYCLE_SECONDS);
-		}
-
-		I_Status_before = inv1->I_Status;
+		printf("Received the values listed below:\n");
+		printf("AC Power: %d\n",I_AC_Power_calculated);
+		printf("AC Voltage: %d\n",inv1->I_AC_VoltageAB);
+		printf("AC Current: %d\n",inv1->I_AC_Current);
+		printf("Heatsink temperature: %d\n",inv1->I_Temp_Heatsink);
 		printf("\n====================================================\n");		
+
+
+		fd = open(path, O_WRONLY);
+		if (fd<=NULL) {
+			fprintf(stderr, "Error while opening file fd: %s\n",strerror(errno));
+		} else {
+			snprintf(buffer,MAXLEN,"1 %d ",I_AC_Power_calculated); // Add Error control, ID variable and timestamp
+			printf("Buffer: %s\n",buffer);
+			write(fd,buffer,sizeof(buffer));
+		}
+		close(fd);
+
+
 		sleep(SE_NORMAL_READ_CYCLE_SECONDS);
 	}
+
+	unlink(path);
 
 	free(inv1);
 
